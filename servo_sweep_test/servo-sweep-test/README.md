439ae3fd5a946d7638135beeb2f4aa5931155c7d
Philipp Küng <git@philippkueng.ch>
2023-09-13 22:15:43 +0200

    add a basic servo sweeper to test if the connected servo is working

---
+++ servo_sweep_test/servo-sweep-test/README.md
@@ -0,0 +1,2 @@
+An example app that once flashed to a LOLIN D32 PRO and having a servo attached it'll do a sweep.
+In my example it wasn't very smooth but at least it was doing something.
---
+++ servo_sweep_test/servo-sweep-test/src/main.cpp
@@ -0,0 +1,32 @@
+/*
+ * This ESP32 code is created by esp32io.com
+ *
+ * This ESP32 code is released in the public domain
+ *
+ * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-servo-motor
+ */
+
+#include <ESP32Servo.h>
+
+#define SERVO_PIN 26 // ESP32 pin GPIO26 connected to servo motor
+
+Servo servoMotor;
+
+void setup() {
+  servoMotor.attach(SERVO_PIN);  // attaches the servo on ESP32 pin
+}
+
+void loop() {
+  // rotates from 0 degrees to 180 degrees
+  for (int pos = 0; pos <= 180; pos += 1) {
+    // in steps of 1 degree
+    servoMotor.write(pos);
+    delay(15); // waits 15ms to reach the position
+  }
+
+  // rotates from 180 degrees to 0 degrees
+  for (int pos = 180; pos >= 0; pos -= 1) {
+    servoMotor.write(pos);
+    delay(15); // waits 15ms to reach the position
+  }
+}
