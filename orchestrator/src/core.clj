23fa86e12d8fe4e86006ce9d9e51324fc2ee59c6
Philipp Küng <git@philippkueng.ch>
2024-03-05 08:37:04 +0100

    exploratory code to talk to the controller controlling the servo and the stepper motor plus talking to the ESP32-CAM to capture pictures and retrieve them

---
+++ orchestrator/src/core.clj
@@ -0,0 +1,92 @@
+(ns core
+  (:require [clj-http.client :as client]
+            [cheshire.core :as json]
+            [clojure.java.io :as io]))
+
+;(defn- fetch-photo!
+;  "makes an HTTP request and fetches the binary object"
+;  [url]
+;  (let [req (client/get url {:as :byte-array :throw-exceptions false})]
+;    (if (= (:status req) 200)
+;      (:body req))))
+
+#_(defn- save-photo!
+  "downloads and stores the photo on disk"
+  [photo]
+  (let [p (fetch-photo! (:url photo))]
+    (if (not (nil? p))
+      (with-open [w (io/output-stream (str "photos/" (:id photo) ".jpg"))]
+        (.write w p)))))
+
+;(defn- save-photo!
+;  "downloads and stores the photo on disk"
+;  [{:keys [url id] :as photo}]
+;  (some-> (fetch-photo! url) (io/copy (io/file "photos" id ".jpg"))))
+
+
+(defn take-picture!
+  "Instructs the controller to take a new picture"
+  []
+  (:body (client/get "http://192.168.0.27/capture")))
+
+(defn fetch-photo!
+  []
+  (some-> (:body (client/get
+                   "http://192.168.0.27/saved-photo"
+                   {:as :stream}))
+    (io/copy (io/file "photo.jpg"))))
+
+(comment
+  ;; download the image from an ESP32-CAM
+  ;; using my code from here https://gist.github.com/philippkueng/11377226
+
+  (do
+    (take-picture!)
+    ;; it takes about 3s for the picture to the be ready so it can be read
+    (Thread/sleep 3000)
+    (fetch-photo!))
+
+  ;; takes roughly 0.5s on a good power source
+  (doall
+    (doseq [i (range 100)]
+      (time (some-> (:body (client/get
+                             "http://192.168.0.27/saved-photo"
+                             {:as :stream}))
+              (io/copy (io/file "photo.jpg"))))
+
+      ))
+
+  (fetch-photo! {:url "http://192.168.0.27/saved-photo"
+                 :id "photo"})
+
+
+
+  )
+
+(def controller-ip "192.168.0.250")
+(defn start-stepper-motor! []
+  (->> (client/get (str "http://" controller-ip "/api/start") {:as :json})
+    :body))
+
+(defn stop-stepper-motor! []
+  (->> (client/get (str "http://" controller-ip "/api/stop") {:as :json})
+    :body))
+
+(comment
+  ;; talking to the controller of the stepper motor
+  (->> (client/get "http://192.168.0.250/api/start" {:as :json})
+    :body)
+
+  (->> (client/get "http://192.168.0.250/api/stop" {:as :json})
+    :body)
+
+  (doall
+    (doseq [i (range 10)]
+      (do
+        (start-stepper-motor!)
+        (Thread/sleep 2000)
+        (stop-stepper-motor!)
+        (Thread/sleep 2000))))
+
+  )
+
